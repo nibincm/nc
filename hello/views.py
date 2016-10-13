bc3f4f8a2d36a6b25966e6ed23fcfcf222060af4
nibincm <nibinmutholil@gmail.com>
2016-10-13 15:43:55 +0530

    My poll app, second try to commit

---
+++ hello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'index.html' 
